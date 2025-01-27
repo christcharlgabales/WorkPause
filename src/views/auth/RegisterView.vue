5fa6264300968eb384a3905ca93ecb696ddb7a75
ccqg <gabaleschrist1@gmail.com>
2025-01-27 13:22:02 +0800

    added login & register page

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -14,7 +14,7 @@ function onClick() {
       <v-app-bar
         class="px-3"
         color="grey-lighten-1
-"
+  "
       >
         <v-spacer></v-spacer>
 
@@ -30,13 +30,17 @@ function onClick() {
         <v-container>
           <v-row>
             <v-col cols="12" md="6" class="mx-auto"
-              ><v-card class="mx-auto" prepend-icon="mdi-login" subtitle="Login" elevation="24">
+              ><v-card class="mx-auto" prepend-icon="mdi-account-plus" subtitle="Register">
                 <template v-slot:title>
                   <span class="font-weight-black">Smart Leave</span>
                 </template>
 
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+
+                    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+
                     <v-text-field label="Email" variant="outlined"></v-text-field>
 
                     <v-text-field
@@ -44,16 +48,18 @@ function onClick() {
                       type="password"
                       variant="outlined"
                     ></v-text-field>
+                    <v-text-field
+                      label="Password Confirmation"
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
 
                     <v-btn class="mt-2" type="submit" block>Submit</v-btn>
                   </v-form>
-
                   <v-divider class="my-5"> </v-divider>
                   <h5 class="text-center">
-                    Don't have account?
-                    <RouterLink class="text-primary" to="/register"
-                      >Click here to Register!</RouterLink
-                    >
+                    Already have account?
+                    <RouterLink class="text-primary" to="/">Click here to Login!</RouterLink>
                   </h5>
                 </v-card-text>
               </v-card></v-col
@@ -66,7 +72,7 @@ function onClick() {
         border
         app
         color="grey-lighten-1
-"
+  "
         >2024 - Smart Leave</v-footer
       >
     </v-app>
