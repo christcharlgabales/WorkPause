94fbe67d03e204abebcdf81ca79c59a37c5cb8f1
ccqg <gabaleschrist1@gmail.com>
2025-01-27 18:13:21 +0800

    Fix login & register validators

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,19 +1,47 @@
 <script setup>
+import { requiredValidator, emailValidator } from '@/utils/validators'
 import { ref } from 'vue'
 
 const visible = ref(false)
+const refVForm = ref()
+
+const formDataDefault = {
+  email: '',
+  password: '',
+}
+
+const formData = ref({
+  ...formDataDefault,
+})
+
+const onSubmit = () => {
+  //   alert(formData.value.email)
+}
+
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit()
+  })
+}
 </script>
 
 <template>
-  <v-form fast-fail @submit.prevent>
-    <v-text-field placeholder="Email address" prepend-inner-icon="mdi-email-outline"></v-text-field>
+  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+    <v-text-field
+      v-model="formData.email"
+      placeholder="Email address"
+      prepend-inner-icon="mdi-email-outline"
+      :rules="[requiredValidator, emailValidator]"
+    ></v-text-field>
 
     <v-text-field
+      v-model="formData.password"
       :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
       :type="visible ? 'text' : 'password'"
       placeholder="Enter your password"
       prepend-inner-icon="mdi-lock-outline"
       @click:append-inner="visible = !visible"
+      :rules="[requiredValidator]"
     ></v-text-field>
 
     <v-btn class="mt-2 bg-primary" type="submit" prepend-icon="mdi-login" block>Login</v-btn>
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,38 +1,84 @@
 <script setup>
+import {
+  requiredValidator,
+  emailValidator,
+  passwordValidator,
+  confirmedValidator,
+} from '@/utils/validators'
 import { ref } from 'vue'
 
+const formDataDefault = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+}
+const formData = ref({
+  ...formDataDefault,
+})
 const visible = ref(false)
 const confirmPasswordVisible = ref(false)
+const refVForm = ref()
+
+const onSubmit = () => {
+  alert(formData.value.email)
+}
+
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit()
+  })
+}
 </script>
 
 <template>
-  <v-form fast-fail @submit.prevent>
+  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
     <v-row>
       <v-col cols="12" sm="6">
-        <v-text-field label="Firstname"></v-text-field>
+        <v-text-field
+          v-model="formData.firstname"
+          label="Firstname"
+          :rules="[requiredValidator]"
+        ></v-text-field>
       </v-col>
 
       <v-col cols="12" sm="6">
-        <v-text-field label="Lastname"></v-text-field>
+        <v-text-field
+          v-model="formData.lastname"
+          label="Lastname"
+          :rules="[requiredValidator]"
+        ></v-text-field>
       </v-col>
     </v-row>
 
-    <v-text-field label="Email"></v-text-field>
+    <v-text-field
+      v-model="formData.email"
+      label="Email"
+      :rules="[requiredValidator, emailValidator]"
+    ></v-text-field>
 
     <v-text-field
+      v-model="formData.password"
       :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
       :type="visible ? 'text' : 'password'"
       placeholder="Password"
       prepend-inner-icon="mdi-lock-outline"
       @click:append-inner="visible = !visible"
+      :rules="[requiredValidator, passwordValidator]"
     ></v-text-field>
 
     <v-text-field
+      v-model="formData.password_confirmation"
       :append-inner-icon="confirmPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
       :type="confirmPasswordVisible ? 'text' : 'password'"
       placeholder="Password Confirmation"
       prepend-inner-icon="mdi-lock-outline"
       @click:append-inner="confirmPasswordVisible = !confirmPasswordVisible"
+      :rules="[
+        requiredValidator,
+        confirmedValidator(formData.password_confirmation, formData.password),
+      ]"
     ></v-text-field>
 
     <v-btn class="mt-2 bg-primary" type="submit" prepend-icon="mdi-account-plus" block
